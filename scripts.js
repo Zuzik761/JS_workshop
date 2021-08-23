755092ad665277e5bc606a89176d9fdf6825827a
Zuzana Davidová <88673168+Zuzik761@users.noreply.github.com>
2021-08-23 20:12:10 +0200

    js_objects

--- scripts.js
+++ scripts.js
@@ -1,4 +1,5 @@
 console.log("zkouska")
+
 let age = 2021-1980
 let WW2 = 1945-1939
 let tesla = 710 * 76
@@ -6,4 +7,16 @@ let secondsOfDay = 60*60*24
 let hungarians = (100*9712887)/7700000000
 let bigger = 9597000-78871
 let participants = 654%7
+
+let names = [`Jana`, `Monika`, `Lenka`, `Katka`]
+console.log(names[2].length)>5
+names.push(`Barbora`)
+names[0]=`Zuzka`
+names.length>4
+
+let post = {
+    field: `colors`,
+}
+let colors = [`red`, `blue`, `white`]
+post.field2 = "hasManyColors"
 
