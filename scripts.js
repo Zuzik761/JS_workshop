a5ae7b0baf2640395186d893ab7f5e0d0d2caafc
Zuzana Davidová <88673168+Zuzik761@users.noreply.github.com>
2021-08-23 19:09:21 +0200

    js_numbers

---
+++ scripts.js
@@ -0,0 +1,9 @@
+console.log("zkouska")
+let age = 2021-1980
+let WW2 = 1945-1939
+let tesla = 710 * 76
+let secondsOfDay = 60*60*24
+let hungarians = (100*9712887)/7700000000
+let bigger = 9597000-78871
+let participants = 654%7
+
